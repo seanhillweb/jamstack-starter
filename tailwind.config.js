2ca55bb873773f74f9f79fb3c39fff2f570dc134
seanhillweb <seanhillweb@gmail.com>
2021-02-01 23:17:10 -0600

    Added variants and plugins for tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,44 @@ module.exports = {
       }
     },
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    display: ['responsive', 'group-hover', 'group-focus', 'hover', 'focus'],
+    opacity: ['responsive', 'group-hover', 'group-focus', 'hover', 'focus'],
+    scale: ['responsive', 'group-hover', 'group-focus', 'hover', 'focus'],
+    textColor: ['responsive', 'group-hover', 'group-focus', 'hover', 'focus'],
+    textDecoration: ['responsive', 'group-hover', 'group-focus', 'hover', 'focus'],
+    transitionProperty: ['responsive', 'group-hover', 'group-focus', 'hover', 'focus'],
+    translate: ['responsive', 'group-hover', 'group-focus', 'hover', 'focus'],
+  },
+  plugins: [
+    plugin(function({ addUtilities, theme }) {
+
+      // https://github.com/tailwindlabs/discuss/issues/196
+      let outlineUtility = {};
+      const outlinePrefix = '2px solid';
+      const colors = theme('colors');
+      Object.keys( colors ).forEach(color => {
+
+        const colorData = colors[color];
+        if(typeof colorData === 'string') {
+          outlineUtility[`.outline-${color}`] = {
+            outline: `${outlinePrefix} ${colorData}`,
+          }
+        }
+        else {
+          Object.keys(colorData).forEach(colorVariation => {
+            outlineUtility[`.outline-${color}-${colorVariation}`] = {
+              outline: `${outlinePrefix} ${colorData[colorVariation]}`,
+            }
+          });
+        }
+      });
+
+      addUtilities(outlineUtility, {
+        variants: ['responsive', 'focus']
+      })
+    })
+  ],
   purge: process.env.NODE_ENV === 'production' ? {
     enabled: true,
     content: ['src/**/*.njk', 'src/**/*.js'],
